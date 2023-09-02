119fb2274f3b27a3090c5b9735ff9ba779fcf11a
Alish-10 <alishdahal77@gmail.com>
2023-09-02 14:13:22 +0545

    wip on front page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
+import AboutSection from "@/components/AboutSection";
+import HeroSection from "@/components/HeroSection";
+import ProjectsSection from "@/components/ProjectSection";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="mx-auto max-w-3xl px-4 sm:px-6 md:max-w-5xl ">
-      {/* <HeroSection />
+      <HeroSection />
       <AboutSection />
-      <ProjectsSection /> */}
+      <ProjectsSection />
     </main>
   );
 }
